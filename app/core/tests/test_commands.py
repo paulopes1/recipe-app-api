9c888ae61cc39765c143474bae2525772cb8ad9c
Paulo Lopes <paulolopesplj@gmail.com>
2026-03-02 21:38:58 -0300

    fix: resolve flake8 issues in db wait command and tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,9 +10,11 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
@@ -22,12 +24,14 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check): 
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]
-        
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
+        patched_sleep.assert_called_with(1)
         patched_check.assert_called_with(databases=['default'])
